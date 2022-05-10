b03eb0e3df56239316ad4406bd3da9c951b688f3
Denis Proshutinskii <jl0pd@outlook.com>
2022-05-10 22:00:25 +0700

    Show IL offset and operands

--- src/jl0pd.Reflection.LightDecompiler/LightDecompiler.cs
+++ src/jl0pd.Reflection.LightDecompiler/LightDecompiler.cs
@@ -1,16 +1,19 @@
 ﻿namespace jl0pd.Reflection;
 
 using System.Buffers.Binary;
+using System.Globalization;
 using System.Reflection;
 using System.Reflection.Emit;
+
+internal sealed record Instruction(int Offset, OpCode OpCode, object? Operand);
 
 public static class LightDecompiler
 {
     public static void PrintInstructions(MethodInfo methodInfo)
         => WriteInstructions(methodInfo, Console.Out);
 
-    public static void PrintInstructions(ReadOnlySpan<byte> il)
-        => WriteInstructions(il, Console.Out);
+    public static void PrintInstructions(ReadOnlySpan<byte> il, Module module)
+        => WriteInstructions(il, module, Console.Out);
 
     public static void WriteInstructions(MethodInfo methodInfo, TextWriter writer)
     {
@@ -31,13 +34,16 @@ public static class LightDecompiler
             throw new ArgumentException("Cannot get IL byte array", nameof(methodInfo));
         }
 
-        WriteInstructions(il, writer);
+        WriteInstructions(il, methodInfo.Module, writer);
     }
 
-    public static void WriteInstructions(ReadOnlySpan<byte> il, TextWriter writer)
+    public static void WriteInstructions(ReadOnlySpan<byte> il, Module module, TextWriter writer)
     {
+        var instructions = new List<Instruction>();
         for (int i = 0; i < il.Length;)
         {
+            int offset = i;
+
             short id = il[i];
             if (IsPrefix(id))
             {
@@ -50,9 +56,83 @@ public static class LightDecompiler
             }
 
             var opCode = s_instructions[id];
-            i += OperandSize(opCode.OperandType);
+            var operand = ReadOperand(il[i..], i, opCode.OperandType, module, out int size);
+            i += size;
 
-            writer.WriteLine(opCode);
+            instructions.Add(new Instruction(offset, opCode, operand));
+        }
+
+        FormatInstructions(instructions, writer);
+    }
+
+    private static void FormatInstructions(List<Instruction> instructions, TextWriter writer)
+    {
+        int maxSize = instructions.Select(i => i.Offset.ToString(CultureInfo.InvariantCulture).Length).Max();
+
+        var format3 = "{0:D" + maxSize.ToString(CultureInfo.InvariantCulture) + "}:  {1} '{2}'";
+        var format2 = "{0:D" + maxSize.ToString(CultureInfo.InvariantCulture) + "}:  {1}";
+
+        foreach (var instruction in instructions)
+        {
+            if (instruction.Operand is { })
+            {
+                writer.WriteLine(format3, instruction.Offset, instruction.OpCode, instruction.Operand);
+            }
+            else
+            {
+                writer.WriteLine(format2, instruction.Offset, instruction.OpCode);
+            }
+        }
+    }
+
+    private static object? ReadOperand(ReadOnlySpan<byte> span, int offset, OperandType operandType, Module module, out int size)
+    {
+        size = OperandSize(operandType);
+
+        switch (operandType)
+        {
+            case OperandType.InlineBrTarget:
+                return offset + size + BinaryPrimitives.ReadInt32BigEndian(span);
+            case OperandType.InlineI:
+            case OperandType.InlineSwitch:
+                return BinaryPrimitives.ReadInt32BigEndian(span);
+
+            case OperandType.InlineI8:
+                return BinaryPrimitives.ReadInt64BigEndian(span);
+
+            case OperandType.InlineMethod:
+            case OperandType.InlineTok:
+            case OperandType.InlineType:
+            case OperandType.InlineField:
+                return module.ResolveMember(BinaryPrimitives.ReadInt32LittleEndian(span));
+
+            case OperandType.InlineSig:
+                return module.ResolveSignature(BinaryPrimitives.ReadInt32LittleEndian(span));
+
+            case OperandType.InlineString:
+                return module.ResolveString(BinaryPrimitives.ReadInt32BigEndian(span));
+
+            case OperandType.InlineNone:
+                return null;
+
+            case OperandType.InlineR:
+                return BinaryPrimitives.ReadDoubleBigEndian(span);
+
+            case OperandType.InlineVar:
+                return BinaryPrimitives.ReadInt16LittleEndian(span);
+
+            case OperandType.ShortInlineBrTarget:
+                return span[0] + offset + size;
+
+            case OperandType.ShortInlineVar:
+            case OperandType.ShortInlineI:
+                return span[0];
+
+            case OperandType.ShortInlineR:
+                return BinaryPrimitives.ReadSingleBigEndian(span);
+
+            default:
+                throw new NotSupportedException();
         }
     }
 
