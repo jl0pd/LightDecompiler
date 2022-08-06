14a9d43c1f9680d08b8131e72ecf077e1b48a788
Denis Proshutinskii <jl0pd@outlook.com>
2022-08-06 12:31:28 +0700

    Update TestApp

--- src/TestApp/Program.cs
+++ src/TestApp/Program.cs
@@ -1,4 +1,20 @@
-﻿using System.Reflection;
+﻿using jl0pd.Reflection;
+using System.Reflection;
 
-MethodInfo equals = typeof(object).GetMethod("Equals", BindingFlags.Public | BindingFlags.Static)!;
-jl0pd.Reflection.LightDecompiler.PrintInstructions(equals);
+
+var curMeth = MethodBase.GetCurrentMethod()!;
+var ty = curMeth.DeclaringType!.GetTypeInfo();
+var meth = ty.DeclaredMethods.First(m => m.Name.Contains(nameof(PrintHw)));
+
+var instr = jl0pd.Reflection.LightDecompiler.Decompile(meth);
+new MsilInstructionFormatter().Format(instr, Console.Out);
+
+static void PrintHw()
+{
+    Console.WriteLine(ToString((object)"qwe"));
+}
+
+static string ToString<T>(T value)
+{
+    return value.ToString();
+}
